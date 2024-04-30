e41201e806da9bd29f4830ba3479040394f0fb80
Khalid Obaide <sayed.khalid.obaide@gmail.com>
2024-04-30 19:18:16 +0300

    Tweak learning parameters

--- train.py
+++ train.py
@@ -2,9 +2,9 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback 
 from gym_env import setup_sample_env
 
-checkpoint_callback = CheckpointCallback(save_freq=100_000, save_path="./checkpoints/")
+checkpoint_callback = CheckpointCallback(save_freq=25_000, save_path="./checkpoints/")
 
 env = setup_sample_env()
 model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log="./logs/")
-model.learn(total_timesteps=10_000_000, callback=checkpoint_callback)
+model.learn(total_timesteps=1_000_000, callback=checkpoint_callback)
 
